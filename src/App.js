ca4057b1a5ff51cde50d808697b2fce14a12bd35
ianbunn <ianrichardbunn@gmail.com>
2019-03-01 17:50:11 -0600

    basic Wrapper component fixed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Wrapper from './components/Wrapper'
-import Header from './components/Header'
-import Instructions from './components/Instructions'
-import Game from './components/Game'
+// import Header from './components/Header'
+// import Instructions from './components/Instructions'
+// import Game from './components/Game'
 
 class App extends Component {
   render() {
     return (
       <Wrapper>
-        <Header></Header>
-        <Instructions></Instructions>
-        <Game></Game>
+        {/* <Header></Header> */}
+        {/* <Instructions></Instructions>
+        <Game></Game> */}
       </Wrapper>
     );
   }
