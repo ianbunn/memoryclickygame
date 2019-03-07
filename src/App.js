44519c7ffe4a8ea9bf9da3040b0c5aa4cb6eccb7
ianbunn <ianrichardbunn@gmail.com>
2019-03-07 09:49:57 -0600

    adding score logic, working with header component

--- src/App.js
+++ src/App.js
@@ -16,30 +16,73 @@ class App extends Component {
   };
   // CHECK FOR CLICKED VALUE
   hasBeenClicked = id => {
-    const { clickedCards } = this.state
+    // DECONSTRUCTING THE STATE
+    const { clickedCards, currentScore, topScore } = this.state
+
+    // GETTING CLICKED CARD'S ID
     let clickedId = id
+
+    // STARTING CURRENT SCORE
+    this.setState({currentScore: currentScore + 1})
+
+    // SHUFFLE CARDS AFTER FIRST CLICK
+    this.shuffleCards()
+
+    // CHECK IF CARD IS IN CLICKED CARDS ARRAY
     if (clickedCards.indexOf(clickedId) > -1) {
-      this.gameOver()
-      alert("game over")
-    } else {
+      
+      // CHECK IF CURRENT SCORE IS GREATER THAN TOP SCORE
+      if (currentScore > topScore) {
+        // IF SO, SET CURRENT SCORE AS TOP SCORE
+        this.setState({topScore: currentScore})
+      }
+      // IF IN CLICKED CARDS ARRAY, RUN GAME OVER!
+      this.gameOver(currentScore)
+      // ALERT GAME OVER WITH CURRENT SCORE AND TOP SCORE
+      // CHECK IF CURRENT SCORE IS GREATER THAN TOP SCORE
+      if(currentScore > topScore) {
+        // IF SO, USE CURRENT SCORE AS TOP SCORE TO ALERT
+        alert(`GAME OVER: Current score: ${currentScore}, Top Score: ${currentScore}`)
+      } else {
+        // IF NOT, USE TOP SCORE TO ALERT
+        alert(`GAME OVER: Current score: ${currentScore}, Top Score: ${topScore}`)
+      }
+    } else if(currentScore < 11) {
+      // IF NOT IN CLICKED CARDS ARRAY, PUSH CARD TO CLICKED CARDS ARRAY
       clickedCards.push(id)
+      // CHECK IF CURRENT SCORE IS GREATER THAN TOP SCORE
+      if(currentScore > topScore) {
+        // IF SO, INCREMENT CURRENT SCORE AND SET CURRENT SCORE AS TOP SCORE
+        this.setState({currentScore: currentScore + 1})
+        this.setState({ topScore: currentScore})
+      }
+      // RUN SHUFFLE CARDS
       this.shuffleCards()
-      console.log(clickedCards)
     }
   };
-  // SHUFFLE CARDS AFTER EVERY CLICK
+
+  // SHUFFLE CARDS WITH FISHER-YATES ALGO
   shuffleCards = ()=> {
     const cards = [].concat(this.state.cards)
-    console.log(cards)
     for (let i = cards.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
       [cards[i], cards[j]] = [cards[j], cards[i]];
     }
     this.setState({ cards });
   }
+
   // GAME OVER
-  gameOver = ()=> {
+  gameOver = (currentScore)=> {
+    if(currentScore > 11) {
+      this.setState({
+        // SET CURRENT SCORE TO 0 AND CLEAR OUT THE CLICKED CARDS ARRAY
+        currentScore: 0,
+        clickedCards: []
+      })
+      return alert("Game won!")
+    }
     this.setState({
+      // SET CURRENT SCORE TO 0 AND CLEAR OUT THE CLICKED CARDS ARRAY
       currentScore: 0,
       clickedCards: []
     })
@@ -48,7 +91,10 @@ class App extends Component {
   render() {
     return (
       <Wrapper>
-        <Header>NFL Collectible Cards</Header>
+        <Header 
+          currentScore={this.state.currentScore}
+          topScore={this.state.topScore}
+        />
         {this.state.cards.map(card => (
           <Card
             hasBeenClicked={this.hasBeenClicked}
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./style.css";
 
-function Header(props) {
-    return <h1 className="title">{props.children}</h1>;
-}
+// function Header(props) {
+//     return (
+//         <div>
+//             <h4 className="title">Current Score:{props.currentScore}</h4>
+//             <h4 className="title">Top Score:{props.topScore}</h4>
+//         </div>
+//     )
+// }
+
+const Header = props => (
+    <div className="navbar">
+        <h4 className="title">Current Score:{props.currentScore}</h4>
+        <h4 className="title">Top Score:{props.topScore}</h4>
+    </div>
+);
 
 export default Header;
