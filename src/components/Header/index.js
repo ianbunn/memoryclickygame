5461a8ae6b7de41036beeb1b00f65dcc8a1bd93f
ianbunn <ianrichardbunn@gmail.com>
2019-03-12 15:40:14 -0500

    changed ref to brand in navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 const Header = props => (
     <nav className="navbar">
         <ul className="nav">
-            <li className="brand"><a href="/"><h4>NFL Clicky Game</h4></a></li>
+            <li className="brand"><a href="/memoryclickygame"><h4>NFL Clicky Game</h4></a></li>
             <span className="scores">
                 <li>Current Score: {props.currentScore}</li>
                 <li>Top Score: {props.topScore}</li>
